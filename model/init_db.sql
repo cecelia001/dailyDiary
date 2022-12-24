a5df5d866fc70482a9f58f9221094a921f2205df
cecelia001 <diascece@gmail.com>
2022-12-23 21:14:37 -0500

    Add initial data to db

--- model/init_db.sql
+++ model/init_db.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS diary;
 CREATE TABLE `days` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     `date` DATE,
-    `entry` VARCHAR(1500),
-    `accomplish` VARCHAR(255),
+    `entry` VARCHAR(255),
+    `accomplish` VARCHAR(255)
 ); 
-INSERT INTO `days` (date, entry, accomplish)
+INSERT INTO `days` (`date`, `entry`, `accomplish`)
     VALUES ('2022-12-21', 'I finished Christmas shopping today', 'finish Christmas shopping, do laundry'), ('2022-12-22', 'I was feeling productive today', 'watch lessons, visit the library');
