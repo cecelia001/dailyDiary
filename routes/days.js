f41fbce6fca123599d1a326e99bfcc54f6938d9b
cecelia001 <diascece@gmail.com>
2022-12-23 21:40:03 -0500

    Add POST route

--- routes/days.js
+++ routes/days.js
@@ -7,7 +7,7 @@ router.get('/', async function(req, res, next) {
     let result = await db(`SELECT * FROM days`);
     let days = result.data;
     if (days.length === 0){
-        res.status(404).send({ error: "yooooo"})
+        res.status(404).send({ error: "Day does not exist"})
     } else {
         res.send(days)
     }
@@ -15,6 +15,24 @@ router.get('/', async function(req, res, next) {
     res.status(500).send({ error: err.message });
  }
 });
+
+router.post('/', async function(req, res) {
+let { date, entry, accomplish } = req.body;
+
+let sql = `
+    INSERT INTO days (date, entry, accomplish)
+    VALUES ('${date}', '${entry}', '${accomplish}')
+    `;
+
+    try {
+       await db (sql);
+       let result = await db(`SELECT * FROM days`);
+       let days = result.data;
+       res.status(201).send(days);
+    } catch(err) {
+       res.status(500).send({ error: err.message });
+    }
+   });
 
 
 module.exports = router;
