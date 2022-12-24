905a8ce751546741b21afb72a00fc4505db34ac6
cecelia001 <diascece@gmail.com>
2022-12-23 22:39:11 -0500

    Create days state in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import AddEntry from "./components/AddEntry.js"
+
+
 function App() {
+
+  const [days, setDays]= useState([])
+
+  useEffect(()=> {
+    getDays();
+  }, []);
+
+  async function getDays(){
+    try {
+      let response = await fetch ('/days');
+      if (response.ok){
+        let data = await response.json();
+        setDays(data);
+      } else {
+        console.log(`server error: ${response.status}: ${response.statusText}`);
+      } 
+      } catch(err){
+        console.log(`network error: ${err.message}`)
+      }
+  };
+
+
   return (
     <div className="App">
-      hello world!
+      
     </div>
   );
 }
