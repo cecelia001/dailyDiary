72569b61a23ff9d79fcbbd47d70326a7a5747161
cecelia001 <diascece@gmail.com>
2022-12-23 22:02:15 -0500

    Add DELETE route

--- routes/days.js
+++ routes/days.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require("../model/helper");
 
+//Get all days from diary
 router.get('/', async function(req, res, next) {
  try {
     let result = await db(`SELECT * FROM days`);
@@ -16,6 +17,8 @@ router.get('/', async function(req, res, next) {
  }
 });
 
+
+//Add a day to diary
 router.post('/', async function(req, res) {
 let { date, entry, accomplish } = req.body;
 
@@ -34,5 +37,23 @@ let sql = `
     }
    });
 
+//delete diary entry
+router.delete('/:id', async function (req, res){
+let id = req.params.id;
+
+try {
+    let result = await db(`SELECT * FROM days WHERE id=${id}`);
+    if ( result.data.length === 0){
+        res.status(404).send({ error: "Day not found"})
+    } else {
+        await db(`DELETE FROM days WHERE id=${id}`);
+        let result = await db(`SELECT * FROM days`);
+        let days = result.data;
+        res.status(201).send(days);
+    }
+} catch(err) {
+    res.status(500).send({ error: err.message });
+}
+});
 
 module.exports = router;
