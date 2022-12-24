d8855452f70bfeb0ada3728c4f84c8a34f336d65
cecelia001 <diascece@gmail.com>
2022-12-24 13:37:22 -0500

    Add form

--- client/src/App.js
+++ client/src/App.js
@@ -26,13 +26,36 @@ function App() {
       }
   };
 
+  async function addEntry(formData){
+    let options = {
+      method: "POST",
+      headers: { "content-Type" : "application/json" },
+      body: JSON.stringify({
+          date: formData.date,
+          entry: formData.entry,
+          accomplish: formData.accomplish,
+      })
+  };
+
+    try{
+      let response = await fetch ('/days', options)
+      if (response.ok){
+        let data = await response.json();
+        setDays(data);
+      } else {
+        console.log(`server error: ${response.status}: ${response.statusText}`);
+      }
+    } catch(err) {
+      console.log(`network error: ${err.message}`)
+    }
+  }
 
   return (
     <div className="App">
       <nav> Add Nav Bar</nav>
       <h1>dailyDiary</h1>
       <h2> Daily inspiration quote</h2>  {/* make a component */}
-      <AddEntry days={days} />
+      <AddEntry days={days} addEntryCb={addEntry} />
     </div>
   );
 }
--- client/src/components/AddEntry.js
+++ client/src/components/AddEntry.js
@@ -1,14 +1,62 @@
 import React, {useState} from "react";
 import "./AddEntry.css"
 
+let BLANK_FORM= {
+    date: "",
+    entry: "",
+    accomplish: ""
+}
 
 function AddEntry(props) {
 
+    const [ formData, setFormData ] = useState(BLANK_FORM)
+
+    function handleChange(event){
+        let { name, value }= event.target;
+        setFormData((data) => ({...data, [name]: value}));
+    };
+
+    function handleSubmit(event){
+        event.preventDefault();
+        props.addEntry(formData);
+        setFormData(BLANK_FORM)
+    };
+
     return (
         <div className="AddEntry">
-            <h2> Add an entry (will go here) </h2>
-            <form>
+            <form onSubmit={handleSubmit}>
+            <label>
+                date
+                <input 
+                    type="text" 
+                    name="date"
+                    value={formData.date}
+                    onChange= {handleChange}
+                />
+            </label>
 
+            <label>
+                entry
+                <textarea
+                    type="text" 
+                    name="entry"
+                    value={formData.entry}
+                    onChange= {handleChange}
+                >
+                </textarea>
+            </label>
+
+            <label>
+                What did you do/Want to do today?
+                <input 
+                    type="text" 
+                    name="accomplish"
+                    value={formData.accomplish}
+                    onChange={handleChange}
+                />
+            </label>
+
+            <button type="submit">Submit</button>
             </form>
         </div>
     );
