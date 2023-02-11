9f71c61e5b625669b2a4d7604108a9ad6b53a7bd
cecelia001 <diascece@gmail.com>
2023-02-11 09:17:04 -0500

    Css changes to NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,9 +7,9 @@ function NavBar(props) {
     return (
         <nav className="NavBar">
             <ul>
-                <li><NavLink to="" end>Home</NavLink></li>
-                <li><NavLink to="/entry">New Entry</NavLink></li>
-                <li><NavLink to="/diary">Read Diary</NavLink></li>
+                <li><NavLink to="" end>home</NavLink></li>
+                <li><NavLink to="/diary">diary</NavLink></li>
+                <li><NavLink to="/entry">new entry</NavLink></li>
             </ul>
         </nav>
     );
