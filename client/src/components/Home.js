15d5ec29b1547ea107002e0a102b765cba8e3e69
cecelia001 <diascece@gmail.com>
2023-02-21 17:39:43 -0500

    Add images to carousel

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,13 +8,13 @@ function Home() {
             <div id="carouselExample" className="carousel slide">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                    <img src="..." className="d-block w-100" alt="..."/>
+                    <img src="https://images.unsplash.com/photo-1551233378-136a5b5564a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="d-block w-100" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                    <img src="..." className="d-block w-100" alt="..."/>
+                    <img src="https://images.unsplash.com/photo-1599008633840-052c7f756385?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" className="d-block w-100" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                    <img src="..." className="d-block w-100" alt="..."/>
+                    <img src="https://images.unsplash.com/photo-1581816163783-7d868ba11739?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="d-block w-100" alt="..."/>
                     </div>
                 </div>
 
