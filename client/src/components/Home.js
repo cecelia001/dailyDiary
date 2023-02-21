c36b9cf2037e6b5f53a3757cdff03fd4b5bb1060
cecelia001 <diascece@gmail.com>
2023-02-21 17:52:38 -0500

    Adding methods to carousel

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,9 +3,20 @@ import "./Home.css"
 
 
 function Home() {
+
+    // const myCarouselElement = document.querySelector('#carouselHome')
+
+    // const carousel = new bootstrap.Carousel(myCarouselElement, {
+    // interval: 2000,
+    // touch: false
+    // })
+
+
+
+
     return (
         <div className="Home">
-            <div id="carouselExample" className="carousel slide">
+            <div id="carouselHome" className="carousel slide">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -23,11 +34,11 @@ function Home() {
                     </div>
                 </div>
 
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselHome" data-bs-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Previous</span>
                 </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselHome" data-bs-slide="next">
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Next</span>
                 </button>
