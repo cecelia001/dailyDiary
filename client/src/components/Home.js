121f8ef912cabbca05f71711f3e5d05372e20fa8
cecelia001 <diascece@gmail.com>
2023-02-21 17:41:58 -0500

    Add indicators to carousel

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,6 +6,11 @@ function Home() {
     return (
         <div className="Home">
             <div id="carouselExample" className="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                     <img src="https://images.unsplash.com/photo-1551233378-136a5b5564a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="d-block w-100" alt="..."/>
@@ -27,7 +32,6 @@ function Home() {
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
-
         </div>
     );
 }
