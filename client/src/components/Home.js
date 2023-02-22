b15e78ba8b6ce25783b29f8b059a7307b757807d
cecelia001 <diascece@gmail.com>
2023-02-22 15:51:52 -0500

    Add bootstrap

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
 
     return (
         <div className="Home">
-            <div className="card" style="width: 18rem;">
+            <div className="card" style={{width: 18+ 'rem'}}>
                 <img src="https://images.unsplash.com/photo-1551233378-136a5b5564a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="card-img-top" alt="Diary"/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -16,7 +16,7 @@ function Home() {
                 </div>
             </div>
 
-            <div className="card" style="width: 18rem;">
+            <div className="card" style={{width: 18+ 'rem'}}>
                 <img src="https://images.unsplash.com/photo-1599008633840-052c7f756385?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" className="card-img-top" alt="Diary"/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -25,7 +25,7 @@ function Home() {
                 </div>
             </div>
 
-            <div className="card" style="width: 18rem;">
+            <div className="card" style={{width: 18+ 'rem'}}>
                 <img src="https://images.unsplash.com/photo-1581816163783-7d868ba11739?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="card-img-top" alt="Diary"/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -34,35 +34,6 @@ function Home() {
                 </div>
             </div>
 
-
-
-            <div id="carouselHome" className="carousel slide">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                    <img src="https://images.unsplash.com/photo-1551233378-136a5b5564a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="d-block w-100" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1599008633840-052c7f756385?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" className="d-block w-100" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1581816163783-7d868ba11739?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="d-block w-100" alt="..."/>
-                    </div>
-                </div>
-
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselHome" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselHome" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-            </div>
         </div>
     );
 }
