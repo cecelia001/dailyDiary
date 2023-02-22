35bf85ffe5df44cf5b49dd682b560094cb5742d8
cecelia001 <diascece@gmail.com>
2023-02-22 15:45:07 -0500

    Add cards to homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,21 +4,41 @@ import "./Home.css"
 
 function Home() {
 
-    // const myCarouselElement = document.querySelector('#carouselHome')
-
-    // const carousel = new bootstrap.Carousel(myCarouselElement, {
-    // interval: 2000,
-    // touch: false
-    // })
-
-
-
 
     return (
         <div className="Home">
+            <div className="card" style="width: 18rem;">
+                <img src="https://images.unsplash.com/photo-1551233378-136a5b5564a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="card-img-top" alt="Diary"/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+            <div className="card" style="width: 18rem;">
+                <img src="https://images.unsplash.com/photo-1599008633840-052c7f756385?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" className="card-img-top" alt="Diary"/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+            <div className="card" style="width: 18rem;">
+                <img src="https://images.unsplash.com/photo-1581816163783-7d868ba11739?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="card-img-top" alt="Diary"/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+
+
             <div id="carouselHome" className="carousel slide">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
