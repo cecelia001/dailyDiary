ca02f4a3da6cfb8cdc6a92a551c639ffaf71f1e2
cecelia001 <diascece@gmail.com>
2023-02-22 16:00:39 -0500

    Css styling on cards

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
 
     return (
         <div className="Home">
-            <div className="card" style={{width: 18+ 'rem'}}>
+            <div className="card" style={{width: 30+'rem'}}>
                 <img src="https://images.unsplash.com/photo-1551233378-136a5b5564a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="card-img-top" alt="Diary"/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -16,7 +16,7 @@ function Home() {
                 </div>
             </div>
 
-            <div className="card" style={{width: 18+ 'rem'}}>
+            <div className="card" style={{width: 30+'rem'}}>
                 <img src="https://images.unsplash.com/photo-1599008633840-052c7f756385?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" className="card-img-top" alt="Diary"/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -25,7 +25,7 @@ function Home() {
                 </div>
             </div>
 
-            <div className="card" style={{width: 18+ 'rem'}}>
+            <div className="card" style={{width: 30+'rem'}}>
                 <img src="https://images.unsplash.com/photo-1581816163783-7d868ba11739?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="card-img-top" alt="Diary"/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
